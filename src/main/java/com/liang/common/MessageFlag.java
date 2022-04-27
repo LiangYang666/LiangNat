294f8f14f6f28afa3828108acf0a7e91e92e1323
LiangYang <mail.liangyang@gmail.com>
2022-04-27 18:54:34 +0800

    消息标志

---
+++ src/main/java/com/liang/common/MessageFlag.java
@@ -0,0 +1,12 @@
+package com.liang.common;
+
+/**
+ * @Description: TODO
+ * @Author: LiangYang
+ * @Date: 2022/4/27 下午2:55
+ **/
+public class MessageFlag {
+    public static final Integer eventLogin = 1;
+    public static final Integer eventAccept = 2;
+    public static final Integer eventForward = 3;
+}
