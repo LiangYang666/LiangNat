3fc35f32b629cafc4db426c9b4b9283d97f00fbc
LiangYang <mail.liangyang@gmail.com>
2022-04-27 21:45:24 +0800

    服务端映射字典

---
+++ src/main/java/com/liang/server/ServerMapUtil.java
@@ -0,0 +1,17 @@
+package com.liang.server;
+
+import java.net.Socket;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @Description: 使用到的map字典，映射端口到socket和socket名到socket的关系，便于转发消息时知道是谁连接的
+ * @Author: LiangYang
+ * @Date: 2022/4/27 下午7:49
+ **/
+public class ServerMapUtil {
+
+    static ConcurrentHashMap<Integer, Socket> serverPortMap = new ConcurrentHashMap<>();  // 端口和socket匹配对，指示某些端口是哪些客户端socket想要监听的
+    static ConcurrentHashMap<String, Socket> socketWanSAMap = new ConcurrentHashMap<>();  // 服务端监听的穿透端口(客户端想监听的，例如25901)，
+    // 这样的端口每来一个新连接请求，将socket的名称，map对应的socket， 每次转发消息时携带这个socket的名称字符串，就知道是这个socket发的消息
+
+}
