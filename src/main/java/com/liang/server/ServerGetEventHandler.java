618b7ac4350bdfba2b8f8c73050807a662f7a216
LiangYang <mail.liangyang@gmail.com>
2022-04-28 10:14:58 +0800

    完成事件处理

--- src/main/java/com/liang/server/ServerGetEventHandler.java
+++ src/main/java/com/liang/server/ServerGetEventHandler.java
@@ -1,26 +1,28 @@
 package com.liang.server;
 
+import com.liang.client.ClientMapUtil;
 import com.liang.common.ByteUtil;
 import com.liang.common.MessageFlag;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 
 /**
- * @Description: 处理客户端的连接
+ * @Description: 服务器处理事件
  * @Author: LiangYang
  * @Date: 2022/4/27 下午4:44
  **/
 
 @Slf4j
-class ServerClientHandler implements Runnable{
+class ServerGetEventHandler implements Runnable{
     Socket clientSocket;
 
-    public ServerClientHandler(Socket clientSocket) {
+    public ServerGetEventHandler(Socket clientSocket) {
         this.clientSocket = clientSocket;
     }
 
@@ -44,20 +46,47 @@ class ServerClientHandler implements Runnable{
             int port = ByteUtil.byteArrayToInt(portsBytes, i * 4);
             log.info("Server\t注册并监听映射端口: " + port);
             boolean listenSuccessFlag = false;
+            ServerSocket serverSocket=null;
             try {
-                ServerSocket serverSocket = new ServerSocket(port);
+                serverSocket = new ServerSocket(port);
                 listenSuccessFlag = true;
             } catch (IOException e) {
                 e.printStackTrace();
                 log.info("Server\t监听端口失败: " + port);
             }
             if (listenSuccessFlag) {
-                ServerMapUtil.serverPortMap.put(port, clientSocket);
-                log.info("Server\t监听端口成功: {}, 并开启线程处理端口" ,port);
+                ServerMapUtil.serverPortMap.put(port, clientSocket);    //记录该端口是这个客户端要监听的
+                log.info("Server\t监听端口成功: {}" ,port);
+                new Thread(new ServerListenNewConnectHandler(serverSocket)).start();
                 listenCount++;
             }
         }
         return listenCount;
+    }
+
+    public void forward(InputStream in) throws IOException {
+        int read;
+        read = in.read();
+        if (read == -1) {
+            log.warn("Server\t转发事件异常");
+            return;
+        }
+        int nameCount = read;
+        byte[] nameBytes = new byte[nameCount];
+        read = in.read(nameBytes);
+        if (read != nameCount) {
+            log.warn("Server\t转发事件异常");
+            return;
+        }
+        byte[] bytes = new byte[1024];
+        read = in.read(bytes);
+        if (read == -1) {
+            log.warn("Client\t转发事件异常");
+            return;
+        }
+        Socket socketWan = ServerMapUtil.socketWanMap.get(new String(nameBytes));
+        OutputStream out = socketWan.getOutputStream();
+        out.write(bytes, 0, read);
     }
 
 
@@ -83,10 +112,8 @@ class ServerClientHandler implements Runnable{
                         clientSocket.close();
                     }
                     log.info("Server\t共监听{}个端口",listenCount);
-                } else if(eventIndex == MessageFlag.eventNewConnect){
-
                 } else if (eventIndex == MessageFlag.eventForward){
-
+                    forward(in);
                 } else {
                     log.info("Server\t非正常事件标志 [{}]", eventIndex);
                 }
