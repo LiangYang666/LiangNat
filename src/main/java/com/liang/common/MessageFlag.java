927950b5bb880e853563550d8c9603c902409580
LiangYang <mail.liangyang@gmail.com>
2022-04-29 16:40:34 +0800

    标志位增加关闭连接

--- src/main/java/com/liang/common/MessageFlag.java
+++ src/main/java/com/liang/common/MessageFlag.java
@@ -7,21 +7,36 @@ package com.liang.common;
  **/
 public class MessageFlag {
     // 消息体定义规范 第一个字节为标志位 标志位有以下几种
-    public static final Integer eventLogin = 1;
-    // 标志为1时
+    public static final int eventLogin = 1;
+    // 是客户端登录事件
     // 第2、3、4、5四个个字节表示需要监听端口的总数portCount
     // 之后的字节共有4*portCount 每个字节表示一个想要监听的端口
 
-    public static final Integer eventNewConnect = 2;
-    // 标志为2时
+    public static final int eventNewConnect = 2;
+    // 表示产生新连接事件，例如有用户访问云端的20022号映射端口了，那么就需要处理
     // 第2、3、4、5四个字节表示发生接收连接的端口号port
     // 第6个字节表示后面socket名称字符串的byte数组长度count
     // 之后的count个字节表示socket名称的字节数组
 
-    public static final Integer eventForward = 3;
-    // 标志为3时
+    public static final int eventCloseConnect = 3;
+    // 表示新关闭连接的事件，例如用户断开了云端20022号映射端口了，或者内网22号(被云20022映射的)端口的进程主动断开了
+    // 第2个字节表示后面socket名称字符串的byte数组长度count
+    // 之后的count个字节表示socket名称的字节数组
+
+    public static final int eventForward = 4;
+    // 是转发事件
     // 第2个字节表示 云端socket名称byte数组的长度nameCount
     // 后面nameCount个字节表示云端socket名称
     // 再后面4个字节表示消息内容的长度dataCount，最长为1024长度
     // 之后的dataCount个字节表示socket名称的字节数组
+
+    static public String getComment(int flag){
+        switch(flag) {
+            case eventLogin: return "eventLogin ["+flag+"]";
+            case eventNewConnect: return "eventNewConnect ["+flag+"]";
+            case eventCloseConnect: return "eventCloseConnect ["+flag+"]";
+            case eventForward: return "eventForward ["+flag+"]";
+            default: return "invalid flag ["+flag+"]";
+        }
+    }
 }
