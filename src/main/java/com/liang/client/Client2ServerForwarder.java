9225839397fb5b842bb9f7a1bd4d8dc49ba797e4
LiangYang <mail.liangyang@gmail.com>
2022-04-29 17:09:37 +0800

    客户端支持转发关闭标志

--- src/main/java/com/liang/client/Client2ServerForwarder.java
+++ src/main/java/com/liang/client/Client2ServerForwarder.java
@@ -29,26 +29,32 @@ public class Client2ServerForwarder implements Runnable{
             try {
                 InputStream in = socketLan.getInputStream();
                 int read = in.read(bytes);
-                if (read == -1){
-                    log.info("Client\t转发消息时，输入流结束，端口号为{},socket为{}", socketLan.getLocalPort(),socketLan);
-                    continue;
-//                    socketLan.close();
-                }
-
                 OutputStream out = NatClient.client2serverSocket.getOutputStream(); // TODO 这里是否需要查看client2serverSocket是否已经关闭
-                byte[] remoteSocketWanNames = ClientMapUtil.socketLanMap.get(socketLan);
+                byte[] remoteSocketWanNameBytes = ClientMapUtil.socketLanMap.get(socketLan);
+                if (read == -1){
+                    log.info("Client\t转发消息时，本地端口[{}]输入流结束，关闭该socketWan ->{}", socketLan.getLocalPort(), socketLan);
+                    socketLan.close();
+                    synchronized (out){
+                        out.write(MessageFlag.eventCloseConnect);
+                        out.write(remoteSocketWanNameBytes.length);
+                        out.write(remoteSocketWanNameBytes);
+//                        out.flush();
+                    }
+                    log.info("Client\t转发关闭连接标志，携带[{}]", new String(remoteSocketWanNameBytes));
+                }
                 synchronized (out){
-                    out.write(ByteUtil.intToByte(MessageFlag.eventForward));    // TODO 查看是否可以直接发
-                    out.write(ByteUtil.intToByte(remoteSocketWanNames.length));
-                    out.write(remoteSocketWanNames);
+                    out.write(MessageFlag.eventForward);    // TODO 查看是否可以直接发
+                    out.write(remoteSocketWanNameBytes.length);
+                    out.write(remoteSocketWanNameBytes);
                     out.write(ByteUtil.intToByteArray(read));
                     out.write(bytes, 0, read);
-                    out.flush();
+//                    out.flush();
                 }
-                log.trace("Client\t本地端口转发消息成功，消息体长度{}，转发携带{}，转发至{}",read, new String(remoteSocketWanNames), NatClient.client2serverSocket);
+                log.trace("Client\t本地端口转发消息成功，消息体长度{}，转发携带{}，转发至{}",read, new String(remoteSocketWanNameBytes), NatClient.client2serverSocket);
 
             } catch (IOException e) {
                 e.printStackTrace();
+                break;
             }
         }
         log.info("Client\t本次穿透连接关闭，socket->{}", socketLan);
