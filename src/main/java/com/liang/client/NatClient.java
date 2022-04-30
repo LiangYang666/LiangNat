b2bbf8721dc3e290b2875ffa76a08d1e95fa78bf
LiangYang <mail.liangyang@gmail.com>
2022-04-30 11:06:05 +0800

    支持Client能够失败重试

--- src/main/java/com/liang/client/NatClient.java
+++ src/main/java/com/liang/client/NatClient.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.ConnectException;
 import java.net.Socket;
 import java.util.ArrayList;
 
@@ -28,22 +29,34 @@ public class NatClient {
         for (int i = 0; i < portWantMap.size(); i++) {
             ClientMapUtil.remotePortMap.put(portWantMap.get(i).getRemotePort(), portWantMap.get(i));
         }
-        try {
-            client2serverSocket = new Socket(serverAddress, serverPort);
-            log.info("Client\t连接远程服务器 socket-> "+client2serverSocket);
-            log.info("Client\t服务端:"+serverAddress+" 端口:"+serverPort);
-            log.info("Client\t客户端:"+client2serverSocket.getLocalAddress()+" 端口:"+client2serverSocket.getLocalPort());
-            OutputStream out = client2serverSocket.getOutputStream();
-            out.write(ByteUtil.intToByte(1));
-            out.write(ByteUtil.intToByteArray(clientConfig.portMap.size()));
-            for (int i = 0; i < clientConfig.portMap.size(); i++) {
-                out.write(ByteUtil.intToByteArray(clientConfig.portMap.get(i).remotePort));
+        while(true){
+            try {
+                client2serverSocket = new Socket(serverAddress, serverPort);
+                log.info("Client\t成功登录到远程服务器 socket-> "+client2serverSocket);
+                log.info("Client\t服务端:"+serverAddress+" 端口:"+serverPort);
+                log.info("Client\t客户端:"+client2serverSocket.getLocalAddress()+" 端口:"+client2serverSocket.getLocalPort());
+                OutputStream out = client2serverSocket.getOutputStream();
+                out.write(ByteUtil.intToByte(1));
+                out.write(ByteUtil.intToByteArray(clientConfig.portMap.size()));
+                for (int i = 0; i < clientConfig.portMap.size(); i++) {
+                    out.write(ByteUtil.intToByteArray(clientConfig.portMap.get(i).remotePort));
+                }
+                Thread workThread = new Thread(new ClientGetEventHandler(client2serverSocket));
+                workThread.start(); // 开启事件监听
+                workThread.join();
+            }catch (ConnectException e) {
+                log.warn("目标"+e.getMessage());
+                e.printStackTrace();
+            }catch (IOException | InterruptedException e) {
+                e.printStackTrace();
             }
-            out.flush();
-            new Thread(new ClientGetEventHandler(client2serverSocket)).start(); // 开启事件监听
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            int sleepSecond = 5;
+            log.info("Client\t{}秒后尝试重新登录到服务器", sleepSecond);
+            try {
+                Thread.sleep(sleepSecond*1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
