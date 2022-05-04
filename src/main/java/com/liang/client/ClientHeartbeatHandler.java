d6088ea24f7e8b78c56793109799d41988eae7ec
LiangYang <mail.liangyang@gmail.com>
2022-05-04 14:02:12 +0800

    优化心跳检测

--- src/main/java/com/liang/client/ClientHeartbeatHandler.java
+++ src/main/java/com/liang/client/ClientHeartbeatHandler.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @Description: 心跳检测客户端处理线程
@@ -13,11 +14,11 @@ import java.net.Socket;
  **/
 @Slf4j
 public class ClientHeartbeatHandler implements Runnable{
-    private final HeartbeatUtil heartbeatUtil;
+    private final AtomicLong lastTime;
     private final Socket client2serverSocket;
 
-    public ClientHeartbeatHandler(Socket client2serverSocket, HeartbeatUtil heartbeatUtil) {
-        this.heartbeatUtil = heartbeatUtil;
+    public ClientHeartbeatHandler(Socket client2serverSocket, AtomicLong lastTime) {
+        this.lastTime = lastTime;
         this.client2serverSocket = client2serverSocket;
     }
 
@@ -26,8 +27,8 @@ public class ClientHeartbeatHandler implements Runnable{
         while(true){
             try {
                 Thread.sleep(HeartbeatUtil.listenInterval);
-                long delayMills = System.currentTimeMillis() - heartbeatUtil.getLastHeartbeatTime();
-                log.trace("Client\t延时间隔需要在{}内，检测到间隔为{}", HeartbeatUtil.delayMillsMax, delayMills);
+                long delayMills = System.currentTimeMillis() - lastTime.get();
+                log.trace("Client\t心跳延时间隔需要在{}内，检测到间隔为{}", HeartbeatUtil.delayMillsMax, delayMills);
                 if (delayMills>HeartbeatUtil.delayMillsMax){
                     log.warn("Client\t心跳检测时间超时,，将关闭连接{}", client2serverSocket);
                     break;
